df039cb811f7adff61031f00ab8192d0af65e40e
AnSimonyan <anna.simona1976@gmail.com>
2024-07-01 11:29:00 +0300

    Добавьте файлы проекта.

---
+++ Listes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Listes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> list1 = new List<string>();
+            list1.Add("Hello");
+            list1.Add("world");
+            list1.Add("Hy");
+
+            list1.RemoveAt(0);  
+            list1.Remove("world");
+            list1[0] = "Hello world";
+
+            foreach (string str in list1)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
